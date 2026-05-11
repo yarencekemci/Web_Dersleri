f1b6143244a82d8334e653b74d915dc95d9b76cb
yarencekemci <yarencekemci@gmail.com>
2026-05-11 23:54:07 +0300

    Razor

--- Ders2_MVC_Razor/Controllers/AnasayfaController.cs
+++ Ders2_MVC_Razor/Controllers/AnasayfaController.cs
@@ -6,6 +6,11 @@ namespace Ders2_MVC_Razor.Controllers
     {
         public IActionResult Index()
         {
+            // Razor, ASP.NET Core içinde kullanılan bir web teknolojisidir.
+            // HTML ile C# kodlarını aynı dosyada yazmayı sağlar.
+            // Dinamik web sayfaları oluşturmak için kullanılır.
+            // Razor dosyalarının uzantısı genellikle .cshtml olur.
+            // Sayfa içinde C# kodu yazmak için @ işareti kullanılır.
             return View();
         }
 
