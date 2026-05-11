7eb9f8064b5e0435f8438d73e030cac28f66b031
yarencekemci <yarencekemci@gmail.com>
2026-05-11 22:11:07 +0300

    razor project has been created

---
+++ Ders2_MVC_Razor/Controllers/AnasayfaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ders2_MVC_Razor.Controllers
+{
+    public class AnasayfaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Menu()
+        {
+            return View();
+        }
+    }
+}
