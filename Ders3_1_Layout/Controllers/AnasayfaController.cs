74f7651f42ed25b132d2c7ece6d6e541b2249e8e
yarencekemci <yarencekemci@gmail.com>
2026-05-12 20:49:26 +0300

    Layout 12.05.2026

---
+++ Ders3_1_Layout/Controllers/AnasayfaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ders3_1_Layout.Controllers
+{
+    public class AnasayfaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Ders3_1_Layout/Controllers/HakkimizdaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ders3_1_Layout.Controllers
+{
+    public class HakkimizdaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
