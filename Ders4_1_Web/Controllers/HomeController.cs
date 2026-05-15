8e0225186e9acff4f84197616f0d47e956d61c83
yarencekemci <yarencekemci@gmail.com>
2026-05-15 19:15:43 +0300

    Katmanli mimariler icin projeler olusturuldu.

---
+++ Ders4_1_Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Ders4_1_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ders4_1_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Ders4_1_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ders4_1_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
